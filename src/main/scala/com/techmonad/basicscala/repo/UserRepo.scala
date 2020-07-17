09fc7b4f3585f272fa3cbe49014257424e639f67
Anand Singh <imanandksingh@gmail.com>
2020-07-17 21:15:11 +0200

    Repo code update

--- src/main/scala/com/techmonad/basicscala/repo/UserRepo.scala
+++ src/main/scala/com/techmonad/basicscala/repo/UserRepo.scala
@@ -7,25 +7,30 @@ import com.techmonad.basicscala.repo.DataAccess.DatabaseConnection
 import com.techmonad.basicscala.util.Mappable
 import com.techmonad.basicscala.util.MappableConversion._
 
+import scala.collection.mutable.ListBuffer
 import scala.concurrent.Future
 
 object DataAccess {
-  type DatabaseConnection = Array[User]
+  type DatabaseConnection = ListBuffer[User]
 
   def uuid: UUID                     = UUID.randomUUID()
-  val connection: DatabaseConnection = Array(User(uuid, "A", "a@a.com"), User(uuid, "B", "b@b.com"), User(uuid, "C", "c@c.com"))
+  val connection: DatabaseConnection = ListBuffer(User(uuid, "A", "a@a.com"), User(uuid, "B", "b@b.com"), User(uuid, "C", "c@c.com"))
 }
 
 abstract class UserRepo[F[_]: Mappable] {
-  def all: F[List[User]]
+  def all: Unit => F[List[User]]
   def find: UUID => F[Option[User]]
   def insert: User => F[User]
 }
 
-class UserRepoInterpreter(connection: DatabaseConnection) extends UserRepo[Future] {
-  override def all: Future[List[User]] = Future.successful(connection.toList)
+class UserRepoInterpreter(connection: DatabaseConnection) extends UserRepo {
+  override def all: Unit => Future[List[User]] = _ => Future.successful(connection.toList)
 
   override def find: UUID => Future[Option[User]] = uuid => Future.successful(connection.find(u => u.id == uuid))
 
-  override def insert: User => Future[User] = user => Future.successful { connection ++ Array(user); user }
+  override def insert: User => Future[User] = user => Future.successful { connection += user; user }
+}
+
+object UserRepo {
+  def apply(): UserRepo[Future] = new UserRepoInterpreter(DataAccess.connection)
 }

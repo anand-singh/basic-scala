1ebb18e231e332a180d6742b75e8c2f4819db0cb
Anand Singh <imanandksingh@gmail.com>
2020-07-17 21:17:26 +0200

    UserService interpreter

--- src/main/scala/com/techmonad/basicscala/service/UserService.scala
+++ src/main/scala/com/techmonad/basicscala/service/UserService.scala
@@ -2,13 +2,16 @@ package com.techmonad.basicscala.service
 
 import java.util.UUID
 
-import com.techmonad.basicscala.domain.Request.{HttpRequest, HttpResponse, NewUser}
+import com.techmonad.basicscala.domain.Request.{HttpRequest, HttpResponse, NewUser, TransformReq, TransformRes, WebRequest}
 import com.techmonad.basicscala.domain.User
 import com.techmonad.basicscala.repo.UserRepo
-import com.techmonad.basicscala.util.Mappable
 import com.techmonad.basicscala.util.MappableConversion._
+import com.techmonad.basicscala.util.{Mappable, ScalaLogger}
+import io.scalaland.chimney.dsl._
 
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.chaining._
 
 abstract class UserService[F[_]: Mappable] {
   def findAllUsers: HttpRequest[Unit] => F[HttpResponse[List[User]]]
@@ -16,10 +19,23 @@ abstract class UserService[F[_]: Mappable] {
   def registerUser: HttpRequest[NewUser] => F[HttpResponse[User]]
 }
 
-class UserServiceInterpreter(repo: UserRepo[Future]) extends UserService {
-  override def findAllUsers: HttpRequest[Unit] => Future[HttpResponse[List[User]]] = ???
+class UserServiceInterpreter(repo: UserRepo[Future]) extends UserService with ScalaLogger {
 
-  override def findUserById: HttpRequest[UUID] => Future[HttpResponse[Option[User]]] = ???
+  override def findAllUsers: WebRequest[Unit, List[User]] = transformReq[Unit] andThen repo.all andThen transformRes[List[User]]
 
-  override def registerUser: HttpRequest[NewUser] => Future[HttpResponse[User]] = ???
+  override def findUserById: WebRequest[UUID, Option[User]] = transformReq[UUID] andThen repo.find andThen transformRes[Option[User]]
+
+  override def registerUser: WebRequest[NewUser, User] = transformReq[NewUser] andThen toUser andThen repo.insert andThen transformRes[User]
+
+  //Register User
+  val toUser: NewUser => User = _.into[User].withFieldComputed(_.id, _ => UUID.randomUUID()).transform
+
+  //Transform Req/Res
+  def transformReq[A]: TransformReq[A] = _.tap(r => info(s"Request received ID - [${r.requestId}]")).body
+  def transformRes[A]: TransformRes[A] = _.map(b => HttpResponse[A](UUID.randomUUID(), b).tap(r => info(s"Response - [$r]")))
+
+}
+
+object UserService {
+  def apply(): UserService[Future] = new UserServiceInterpreter(UserRepo())
 }

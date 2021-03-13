a1e777e08c0d70b53b9e792e30df5ec178a540df
Scala Steward <me@scala-steward.org>
2021-03-13 20:11:35 +0100

    Reformat with scalafmt 2.7.5

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
 import sbt._
 
 object Dependencies {
-  lazy val cats      = "org.typelevel"              %% "cats-core"      % "2.0.0"
-  lazy val chimney   = "io.scalaland"               %% "chimney"        % "0.5.3"
-  lazy val logback   = "ch.qos.logback"             % "logback-classic" % "1.2.3"
-  lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"  % "3.9.2"
-  lazy val scalaTest = "org.scalatest"              %% "scalatest"      % "3.1.4" % Test
+  lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.0.0"
+  lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.5.3"
+  lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.2.3"
+  lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.2"
+  lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.1.4" % Test
 }

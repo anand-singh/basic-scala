ca3e76e4af390f315f3c790238efaca70c043585
Anand Singh <imanandksingh@gmail.com>
2021-04-07 17:15:11 +0200

    Build file update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,9 +1,9 @@
 import sbt._
 
 object Dependencies {
-  lazy val cats      = "org.typelevel"              %% "cats-core"      % "2.5.0"
-  lazy val chimney   = "io.scalaland"               %% "chimney"        % "0.6.1"
-  lazy val logback   = "ch.qos.logback"             % "logback-classic" % "1.2.3"
-  lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"  % "3.9.3"
-  lazy val scalaTest = "org.scalatest"              %% "scalatest"      % "3.2.7" % Test
+  lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.5.0"
+  lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.6.1"
+  lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.2.3"
+  lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3"
+  lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.2.7" % Test
 }

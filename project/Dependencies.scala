f754668f42280d455789ce4c1aa4c53d7a70f34d
Anand Singh <imanandksingh@gmail.com>
2021-05-14 00:27:31 +0000

    Update cats-core to 2.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.6.0"
+  lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.6.1"
   lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.6.1"
   lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.2.3"
   lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3"

7cbc7a5e41dcb1d582aa998d20f137d4be6f7933
Anand Singh <imanandksingh@gmail.com>
2021-06-17 00:20:49 +0000

    Update scala-logging to 3.9.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,6 @@ object Dependencies {
   lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.6.1"
   lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.6.1"
   lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.2.3"
-  lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.3"
+  lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.4"
   lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.2.9" % Test
 }

99486781beedd2875c0f97caeb3b6949889a4a2e
Anand Singh <4903911+anand-singh@users.noreply.github.com>
2022-09-20 08:53:07 +0000

    Update scalatest from 3.2.12 to 3.2.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,5 @@ object Dependencies {
   lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.6.1"
   lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.2.11"
   lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5"
-  lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.2.12" % Test
+  lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.2.13" % Test
 }

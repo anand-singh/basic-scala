872251865fef9684ad9edd18a50f5a7e76bcff29
Anand Singh <4903911+anand-singh@users.noreply.github.com>
2022-09-20 08:52:18 +0000

    Update logback-classic from 1.2.11 to 1.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.8.0"
   lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.6.1"
-  lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.2.11"
+  lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.4.1"
   lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5"
   lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.2.12" % Test
 }

47d16919605fa24abd5c978bc238529130edd618
Anand Singh <imanandksingh@gmail.com>
2022-11-13 01:30:45 +0100

    Update cats-core from 2.8.0 to 2.9.0 (#109)
    Co-authored-by: Anand Singh <4903911+anand-singh@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.8.0"
+  lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.9.0"
   lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.4.4"
   lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.6.2"
   lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5"

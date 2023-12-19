5ff480aec07474295fc6ebd2045d0652ac14a0b0
Anand Singh <imanandksingh@gmail.com>
2023-12-19 02:13:40 +0100

    Update chimney from 0.8.3 to 0.8.4 (#119)
    Co-authored-by: Anand Singh <4903911+anand-singh@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Dependencies {
   lazy val cats      = "org.typelevel"              %% "cats-core"       % "2.10.0"
   lazy val logback   = "ch.qos.logback"              % "logback-classic" % "1.4.14"
-  lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.8.3"
+  lazy val chimney   = "io.scalaland"               %% "chimney"         % "0.8.4"
   lazy val logging   = "com.typesafe.scala-logging" %% "scala-logging"   % "3.9.5"
   lazy val scalaTest = "org.scalatest"              %% "scalatest"       % "3.2.17" % Test
 }
